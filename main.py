d2ac2b3bd175b06891b872ef77ed02b753fc70dc
Marvelous Ajuzie <chizurummarvelous14@gmail.com>
2026-02-21 16:05:34 +0100

    working with open ai llm

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ from langchain.llms import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
+
+
+def generate_pet_name():
+    llm = OpenAI(temperature=0.6)
+
+    name = llm("I have a pet dog and i want a cool name for it, sugguest me 10 cool names for my dog")
+
+    return name
+
+if __name__ == "__main__":
+    print(generate_pet_name())
