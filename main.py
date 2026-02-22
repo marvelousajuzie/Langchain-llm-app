60df2f1a66064ad770e2b5a58696f9862616eb34
Marvelous Ajuzie <chizurummarvelous14@gmail.com>
2026-02-22 14:05:41 +0100

    use openrouter api key instaed of gpt

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from langchain.llms import OpenAI
+from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-
 def generate_pet_name():
-    llm = OpenAI(temperature=0.6)
+    llm = ChatOpenAI(
+        model="meta-llama/llama-3-8b-instruct",
+        base_url="https://openrouter.ai/api/v1",
+        api_key=os.getenv("OPENROUTER_API_KEY"),
+        temperature=0.7,
+    )
 
-    name = llm("I have a pet dog and i want a cool name for it, sugguest me 10 cool names for my dog")
+    response = llm.invoke(
+        "I have a pet dog and I want a cool name for it, suggest 10 cool names."
+    )
 
-    return name
+    return response.content
+
 
 if __name__ == "__main__":
     print(generate_pet_name())
